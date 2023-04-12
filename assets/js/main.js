30dce0d92e3e753a134979d2c469ac37ba937c10
Purima <purnimapathak999@gmail.com>
2023-04-12 12:03:59 +0530

    first commit~

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/*!
+=========================================================
+* itechSolution landing page
+=========================================================
+
+* Copyright: 2023 itechSolution
+* Licensed: ()
+* Coded by itechSolution
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
